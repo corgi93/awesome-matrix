65fe7dd26f4c509aa6e9157e2c3c753a631e43c2
JIN <gurwls714@naver.com>
2018-09-20 14:41:52 +0900

    lesson2 - print 2DimensionalArray
    npm start

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 /*
  * lesson1
  */
+
 const hello: string = "Hello, World";
 console.log(hello);
 
 
+/*
+ * lesson2
+ */
+
+function printStarArray(rows: number, cols: number): void {
+    const myarr: Array<Array<string>> = [];
+    for (let r = 0; r < rows; r++) {
+        myarr[r] = [];
+        for (let c = 0; c < cols; c++) {
+            myarr[r][c] = "*";
+        }
+    }
+    console.log(myarr);
+}
+
+printStarArray(4, 5);
 
