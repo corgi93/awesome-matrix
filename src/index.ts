3324070a64952516f67597f674e47fba15f44b10
JIN <gurwls714@naver.com>
2018-09-20 18:05:51 +0900

    lesson2 - square matrix

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
 /*
  * lesson2
  */
+
 import readline from "readline-sync";
 
 
+const inputNumber: any = readline.question("Enter number: ");
 
-const inputRows: any = readline.question("Enter Rows: ");
-const inputCols: any = readline.question("Enter Cols: ");
-
-
-function printStarArray(rows: number, cols: number): Array<Array<string>> {
+function squareMatrix(degree: number): Array<Array<string>> {
     const twoDimensionArray: Array<Array<string>> = [];
-    for (let r = 0; r < rows; r++) {
+    for (let r = 0; r < degree; r++) {
         twoDimensionArray[r] = [];
-        for (let c = 0; c < cols; c++) {
-            twoDimensionArray[r][c] = "*";
+        for (let c = 0; c < degree; c++) {
+            if (c <= r) {
+                twoDimensionArray[r][c] = "*";
+            }else{
+                twoDimensionArray[r][c] = " ";
+            }
         }
     }
     return twoDimensionArray;
 }
 
 
-console.log(printStarArray(inputRows, inputCols));
+console.log(squareMatrix(inputNumber));
