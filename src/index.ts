bdeb1a8b7243884a3415d1e942f6605f6deb6edb
JIN <gurwls714@naver.com>
2018-09-21 11:33:10 +0900

    lesson3 - revised index

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
 /*
  * lesson2
  */
-
 import readline from "readline-sync";
-
 
 const inputNumber: any = readline.question("Enter number: ");
 
-function squareMatrix(degree: number): Array<Array<string>> {
+export function squareMatrix(degree: number): Array<Array<string>> {
     const twoDimensionArray: Array<Array<string>> = [];
     for (let r = 0; r < degree; r++) {
         twoDimensionArray[r] = [];
         for (let c = 0; c < degree; c++) {
             if (c <= r) {
                 twoDimensionArray[r][c] = "*";
-            }else{
+            } else {
                 twoDimensionArray[r][c] = " ";
             }
         }
     }
     return twoDimensionArray;
 }
-
-
 console.log(squareMatrix(inputNumber));
+
