2a0c5c5fbdb959e577f343adc9741723d4a15f9d
JIN <gurwls714@naver.com>
2018-09-28 14:43:35 +0900

    lesson3 - revised code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-/*
- * lesson2
-*/
 import readline from "readline-sync";
 import {squareMatrix} from "./squareMatrix";
 
 export const inputNumber: string = readline.question("Enter number: ");
+let value = Number(inputNumber);
 
-if (isNaN(Number(inputNumber))) {
+if (isNaN(value)) {
     console.log("this is not a number. please enter number");
-} else if (Number(inputNumber)) {
-    Number(inputNumber);}
 
-console.log(squareMatrix(Number(inputNumber)));
+} else if (value) {
+    console.log(squareMatrix(value));
+}
+
