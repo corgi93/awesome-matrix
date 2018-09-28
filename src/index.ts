24651451dabb88678bbb6217a0d4b92ef3f9c3c2
JIN <gurwls714@naver.com>
2018-09-28 16:09:20 +0900

    lesson3 - revised about comment(variable)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import readline from "readline-sync";
 import {squareMatrix} from "./squareMatrix";
 
-export const inputNumber: string = readline.question("Enter number: ");
-let value = Number(inputNumber);
+const inputNumber: string = readline.question("Enter number: ");
+const value = Number(inputNumber);
 
 if (isNaN(value)) {
     console.log("this is not a number. please enter number");
-
 } else if (value) {
     console.log(squareMatrix(value));
 }
