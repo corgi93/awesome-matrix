cfe2db619272acc02e927d501e4d5aebf0474b11
JIN <gurwls714@naver.com>
2018-10-03 02:35:58 +0900

    lesson4 - remove blank

--- src/squareMatrix.ts
+++ src/squareMatrix.ts
@@ -5,7 +5,6 @@ enum MatrixInfo {
     LeftDownwardRightTriangle,
     RightDownwardRightTriangle
 }
-
 export function squareMatrix(degree: number, pattern: MatrixInfo): Array<Array<string>> {
     switch (pattern) {
         case MatrixInfo.LeftUpwardRightTriangle:
@@ -20,7 +19,6 @@ export function squareMatrix(degree: number, pattern: MatrixInfo): Array<Array<s
             return rightDownwardRightTriangle(degree);
     }
 }
-
 //첫 번째 패턴
 function leftUpwardRightTriangle(degree: number): Array<Array<string>> {
     const twoDimensionArray: Array<Array<string>> = [];
@@ -37,7 +35,6 @@ function leftUpwardRightTriangle(degree: number): Array<Array<string>> {
     }
     return twoDimensionArray;
 }
-
 //두 번째 패턴
 function rightUpwardRightTriangle(degree: number): Array<Array<string>> {
     const twoDimensionArray: Array<Array<string>> = [];
@@ -53,41 +50,35 @@ function rightUpwardRightTriangle(degree: number): Array<Array<string>> {
     }
     return twoDimensionArray;
 }
-
 // 세 번쨰 패턴
 function pyramidMatrix(degree: number): Array<Array<string>> {
     const floor = Math.floor(degree / 2);
     const twoDimensionArray: Array<Array<string>> = [];
-
     if (degree % 2 == 0) {
         throw new Error('even degree could not make pyramid');
     }
-
-        for (let r = 0; r < degree; r++) {
-            twoDimensionArray[r] = [];
-            for (let c = 0; c < degree; c++) {
-                const sum = r + c;
-                const sub = c - r;
-
-                if (r <= floor) {
-                    if ((sum >= floor) && (sub <= floor)) {
-                        twoDimensionArray[r][c] = "*";
-                    } else {
-                        twoDimensionArray[r][c] = " ";
-                    }
+    for (let r = 0; r < degree; r++) {
+        twoDimensionArray[r] = [];
+        for (let c = 0; c < degree; c++) {
+            const sum = r + c;
+            const sub = c - r;
+            if (r <= floor) {
+                if ((sum >= floor) && (sub <= floor)) {
+                    twoDimensionArray[r][c] = "*";
                 } else {
                     twoDimensionArray[r][c] = " ";
                 }
+            } else {
+                twoDimensionArray[r][c] = " ";
             }
         }
-
+    }
     return twoDimensionArray;
 }
 
 //네 번째 패턴
 function leftDownwardRightTriangle(degree: number): Array<Array<string>> {
     const twoDimensionArray: Array<Array<string>> = [];
-
     for (let r = 0; r < degree; r++) {
         twoDimensionArray[r] = [];
         for (let c = 0; c < degree; c++) {
@@ -100,11 +91,9 @@ function leftDownwardRightTriangle(degree: number): Array<Array<string>> {
     }
     return twoDimensionArray;
 }
-
 //다섯 번째 패턴
 function rightDownwardRightTriangle(degree: number): Array<Array<string>> {
     const twoDimensionArray: Array<Array<string>> = [];
-
     for (let r = 0; r < degree; r++) {
         twoDimensionArray[r] = [];
         for (let c = 0; c < degree; c++) {
