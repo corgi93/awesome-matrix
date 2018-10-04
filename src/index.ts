3239f240c5b78e340bd9f92221e103f4577e974f
JIN <gurwls714@naver.com>
2018-10-04 17:54:26 +0900

    lesson5 - fixed by tslint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import readline from "readline-sync";
-import { squareMatrix } from "./squareMatrix";
+import readline from 'readline-sync'
+import { squareMatrix } from './squareMatrix'
 
-const inputPattern: string = readline.question("Enter pattern(case 0 - 4): ");
-const inputDegree: string = readline.question("Enter degree: ");
-const inputNumberPattern = Number(inputPattern);
-const inputNumberDegree = Number(inputDegree);
+const inputPattern: string = readline.question('Enter pattern(case 0 - 4): ')
+const inputDegree: string = readline.question('Enter degree: ')
+const inputNumberPattern = Number(inputPattern)
+const inputNumberDegree = Number(inputDegree)
 
 if (isNaN(inputNumberPattern && inputNumberDegree)) {
-    console.log("patterns and degree must be a number.");
-} else if ((inputNumberPattern < 0 || inputNumberPattern > 4)) {
-    console.log("please enter a pattern number between 0 and 4");
+    console.log('patterns and degree must be a number.')
+} else if (inputNumberPattern < 0 || inputNumberPattern > 4) {
+    console.log('please enter a pattern number between 0 and 4')
 } else {
-    console.log(squareMatrix(inputNumberDegree, inputNumberPattern));
+    console.log(squareMatrix(inputNumberDegree, inputNumberPattern))
 }
