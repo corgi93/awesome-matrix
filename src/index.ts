ad31fc24cb04103982e627d754437349564dfa73
JIN <gurwls714@naver.com>
2018-10-04 15:04:10 +0900

    lesson4 - revised condition

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,11 @@ const inputPattern: string = readline.question("Enter pattern(case 0 - 4): ");
 const inputDegree: string = readline.question("Enter degree: ");
 const inputNumberPattern = Number(inputPattern);
 const inputNumberDegree = Number(inputDegree);
+
 if (isNaN(inputNumberPattern && inputNumberDegree)) {
     console.log("patterns and degree must be a number.");
+} else if ((inputNumberPattern < 0 || inputNumberPattern > 4)) {
+    console.log("please enter a pattern number between 0 and 4");
 } else {
-    if(inputNumberPattern < 0 || inputNumberPattern > 4){
-        console.log("please enter a pattern number between 0 and 4");
-    }else{
-        console.log(squareMatrix(inputNumberDegree, inputNumberPattern));
-    }
+    console.log(squareMatrix(inputNumberDegree, inputNumberPattern));
 }
