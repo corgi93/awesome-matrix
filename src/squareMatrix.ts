694200638052aed4d9730f3c0fe541ebfe591180
JIN <gurwls714@naver.com>
2018-10-10 18:41:22 +0900

    lesson7 - edited new line

--- src/squareMatrix.ts
+++ src/squareMatrix.ts
@@ -1,6 +1,7 @@
 export interface IMatrixStrategy {
     twoDimensionArrayLogic(degree: number, stringShape: string | number): string[][]
 }
+
 export class Matrix {
     private readonly shapeCharacter: string | number
     private readonly pattern: IMatrixStrategy
