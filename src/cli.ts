a1e14f10d0558226d07d4b498d9d0353c8dceb91
JIN <gurwls714@naver.com>
2018-10-18 11:45:03 +0900

    lesson9 - revised more user-convenient cli

--- src/cli.ts
+++ src/cli.ts
@@ -7,46 +7,162 @@ import { RightUpwardRightTriangle } from './patterns/rightUpwardRightTriangle'
 import { SquareMatrix } from './squareMatrix'
 
 const argv = process.argv
+const stdin = process.stdin
+const stdout = process.stdout
 
 export function usage(): void {
-    console.log(' Usage: TSAwesomeMatrix ')
-    console.log(' Command line arguments: ')
-    console.log(' [-help | -h : this guidelines] ')
-    console.log(' [-pyramid : print pyramid pattern]')
-    console.log(' [-leftUpward : print leftUpwardRightTriangle pattern]')
-    console.log(' [-rightUpward : print rightUpwardRightTriangle pattern]')
-    console.log(' [-leftDownward : print leftDownwardRightTriangle pattern]')
-    console.log(' [-rightDownward : print rightDownwardRightTriangle pattern]')
-    process.exit()
+    console.log(' Usage: TSAwesomeMatrix CLI')
+    console.log = (writeData: string) => {
+        stdout.write(writeData + ' | ')
+    }
+    console.log(' [-help : this guidelines] ')
+    console.log(' [-pyramid : print pyramid pattern] ')
+    console.log(' [-leftUpward : print leftUpwardRightTriangle pattern] ')
+    console.log(' [-rightUpward : print rightUpwardRightTriangle pattern] ')
+    console.log(' [-leftDownward : print leftDownwardRightTriangle pattern] ')
+    console.log(' [-rightDownward : print rightDownwardRightTriangle pattern] ')
 }
 
-const leftUpwardRightTriangle = new SquareMatrix('&', new LeftUpwardRightTriangle())
-const rightUpwardRightTriangle = new SquareMatrix('8', new RightUpwardRightTriangle())
-const pyramidTriangle = new SquareMatrix(8, new PyramidTriangle())
-const leftDownwardRightTriangle = new SquareMatrix('K9', new LeftDownwardRightTriangle())
-const rightDownwardRightTriangle = new SquareMatrix('=', new RightDownwardRightTriangle())
+function isContain(thisArgv: string[], keyword: string): boolean {
+    return thisArgv.lastIndexOf(keyword) !== -1
+}
+
+function inputPyramid() {
+    stdin.resume()
+    stdin.setEncoding('utf8')
+
+    const inputData = ['Enter Character: ', 'Enter Degree: ']
+    function askData(i: number) {
+        stdout.write(`${inputData[i]}\n`)
+    }
+    stdin.on('data', data => {
+        argv.push(data.replace('\r\n', ''))
+        if (argv.length < 6) {
+            askData(argv.length - 4)
+        } else {
+            process.exit()
+        }
+        const pyramidTriangle = new SquareMatrix(argv[3], new PyramidTriangle())
+        console.log(pyramidTriangle.executePattern(parseInt(argv[4], 10)).join('\n'))
+    })
+    process.on('exit', () => {
+        stdout.write(`Character: ${argv[3]} , Degree: ${argv[4]}`)
+    })
+    askData(0)
+}
+
+function inputLeftUpward() {
+    stdin.resume()
+    stdin.setEncoding('utf8')
+
+    const inputData = ['Enter Character: ', 'Enter Degree: ']
+    function askData(i: number) {
+        stdout.write(`${inputData[i]}\n`)
+    }
+    stdin.on('data', data => {
+        argv.push(data.replace('\r\n', ''))
+        if (argv.length < 6) {
+            askData(argv.length - 4)
+        } else {
+            process.exit()
+        }
+        const pyramidTriangle = new SquareMatrix(argv[3], new LeftUpwardRightTriangle())
+        console.log(pyramidTriangle.executePattern(parseInt(argv[4], 10)).join('\n'))
+    })
+    process.on('exit', () => {
+        stdout.write(`Character: ${argv[3]} , Degree: ${argv[4]}`)
+    })
+    askData(0)
+}
+
+function inputRightUpward() {
+    stdin.resume()
+    stdin.setEncoding('utf8')
+
+    const inputData = ['Enter Character: ', 'Enter Degree: ']
+    function askData(i: number) {
+        stdout.write(`${inputData[i]}\n`)
+    }
+    stdin.on('data', data => {
+        argv.push(data.replace('\r\n', ''))
+        if (argv.length < 6) {
+            askData(argv.length - 4)
+        } else {
+            process.exit()
+        }
+        const pyramidTriangle = new SquareMatrix(argv[3], new RightUpwardRightTriangle())
+        console.log(pyramidTriangle.executePattern(parseInt(argv[4], 10)).join('\n'))
+    })
+    process.on('exit', () => {
+        stdout.write(`Character: ${argv[3]} , Degree: ${argv[4]}`)
+    })
+    askData(0)
+}
+
+function inputLeftDownward() {
+    stdin.resume()
+    stdin.setEncoding('utf8')
+
+    const inputData = ['Enter Character: ', 'Enter Degree: ']
+    function askData(i: number) {
+        stdout.write(`${inputData[i]}\n`)
+    }
+    stdin.on('data', data => {
+        argv.push(data.replace('\r\n', ''))
+        if (argv.length < 6) {
+            askData(argv.length - 4)
+        } else {
+            process.exit()
+        }
+        const pyramidTriangle = new SquareMatrix(argv[3], new LeftDownwardRightTriangle())
+        console.log(pyramidTriangle.executePattern(parseInt(argv[4], 10)).join('\n'))
+    })
+    process.on('exit', () => {
+        stdout.write(`Character: ${argv[3]} , Degree: ${argv[4]}`)
+    })
+    askData(0)
+}
+
+function inputRightDownward() {
+    stdin.resume()
+    stdin.setEncoding('utf8')
+
+    const inputData = ['Enter Character: ', 'Enter Degree: ']
+    function askData(i: number) {
+        stdout.write(`${inputData[i]}\n`)
+    }
+    stdin.on('data', data => {
+        argv.push(data.replace('\r\n', ''))
+        if (argv.length < 6) {
+            askData(argv.length - 4)
+        } else {
+            process.exit()
+        }
+        const pyramidTriangle = new SquareMatrix(argv[3], new RightDownwardRightTriangle())
+        console.log(pyramidTriangle.executePattern(parseInt(argv[4], 10)).join('\n'))
+    })
+    process.on('exit', () => {
+        stdout.write(`Character: ${argv[3]} , Degree: ${argv[4]}`)
+    })
+    askData(0)
+}
 
 if (argv.length === 3) {
-    if (argv.lastIndexOf('-help') !== -1) {
+    if (isContain(argv, '-help')) {
         usage()
-    } else if (argv.lastIndexOf('-pyramid') !== -1) {
-        argv[3] = 'this is pyramid matrix using two-dimensional array'
-        console.log(pyramidTriangle.executePattern(5).join('\n'))
-    } else if (argv.lastIndexOf('-leftUpward') !== -1) {
-        argv[3] = 'this is leftUpward-RightTriangle matrix using two-dimensional array'
-        console.log(leftUpwardRightTriangle.executePattern(7).join('\n'))
-    } else if (argv.lastIndexOf('-rightUpward') !== -1) {
-        argv[3] = 'this is rightUpward-RightTriangle matrix using two-dimensional array'
-        console.log(rightUpwardRightTriangle.executePattern(4).join('\n'))
-    } else if (argv.lastIndexOf('-leftDownward') !== -1) {
-        argv[3] = 'this is leftDownward-RightTriangle matrix using two-dimensional array'
-        console.log(leftDownwardRightTriangle.executePattern(9).join('\n'))
-    } else if (argv.lastIndexOf('-rightDownward') !== -1) {
-        argv[3] = 'this is rightDownward-RightTriangle matrix using two-dimensional array'
-        console.log(rightDownwardRightTriangle.executePattern(6).join('\n'))
+    } else if (isContain(argv, '-pyramid')) {
+        inputPyramid()
+    } else if (isContain(argv, '-leftUpward')) {
+        inputLeftUpward()
+    } else if (isContain(argv, '-rightUpward')) {
+        inputRightUpward()
+    } else if (isContain(argv, '-leftDownward')) {
+        inputLeftDownward()
+    } else if (isContain(argv, '-rightDownward')) {
+        inputRightDownward()
     } else {
         console.log('if you want to see the guidelines. please enter matrix -help')
     }
 } else {
-    throw new Error('please enter one keyword')
+    console.error('please enter only one keyword')
 }
