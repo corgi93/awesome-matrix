b43bfe80f87e891c90c8ee514e13c8991efe0753
JIN <gurwls714@naver.com>
2018-11-06 21:02:31 +0900

    lesson9 - reviesed code

--- src/util/cli.test.ts
+++ src/util/cli.test.ts
@@ -1,7 +1,100 @@
-// import { parsingValue } from './cli'
+import { Argument, IOption, options, removeDash } from './cli'
 
-// describe('extractValue function', () => {
-//     it('should be ... when ...', () => {
-//         expect(parsingValue(IOptions)).toEqual()
-//     })
-// })
+describe('class Argument test', () => {
+    it('should be values when call getValue ', () => {
+        const args = ['node', 'matrix', '-p=pyramid']
+        const opt: IOption[] = [{ name: 'pattern', shortcut: 'p' }]
+        const arg = new Argument(args, opt)
+        expect(arg.getValue('p')).toEqual('pyramid')
+    })
+
+    it('should be values when call getValue ', () => {
+        const args = ['node', 'matrix', '-d=10']
+        const opt: IOption[] = [{ name: 'degree', shortcut: 'd' }]
+        const arg = new Argument(args, opt)
+        expect(arg.getValue('d')).toEqual('10')
+    })
+
+    it('should slice value', () => {
+        expect(removeDash('-p')).toEqual('p')
+    })
+
+    it('should .. when call isValid function', () => {
+        const args = ['node', 'matrix', '-d=10']
+        const opt: IOption[] = [
+            {
+                name: 'pattern',
+                shortcut: 'p',
+                validation: () => {
+                    return { isValid: true }
+                }
+            },
+            {
+                name: 'degree',
+                shortcut: 'd',
+                validation: () => {
+                    return { errorMessage: 'occurred error' }
+                }
+            }
+        ]
+        const arg = new Argument(args, opt)
+        const validCheck = arg.validationCheck
+        expect(validCheck.isValid).toEqual(false)
+        expect(validCheck.errorMessages).toEqual(['occurred error'])
+    })
+
+    it('should be default value when args is not contain option', () => {
+        const args = ['node', 'matrix']
+        const opt: IOption[] = [
+            {
+                defaultValue: 'pyramid',
+                name: 'pattern',
+                shortcut: 'p'
+            }
+        ]
+        const arg = new Argument(args, opt)
+        expect(arg.getDefaultValue('p')).toEqual('pyramid')
+    })
+})
+
+describe('options test', () => {
+    it('should be false when -p= is not pattern', () => {
+        const args = ['node', 'matrix', '-p=hello', '-s=!', '-d=5']
+        const arg = new Argument(args, options)
+        const validCheck = arg.validationCheck
+        expect(validCheck.isValid).toEqual(false)
+        expect(validCheck.errorMessages).toEqual(['check the patterns'])
+    })
+
+    it('should be false when -s= is more than one', () => {
+        const args = ['node', 'matrix', '-p=leftUpward', '-s=@@', '-d=$']
+        const arg = new Argument(args, options)
+        const validCheck = arg.validationCheck
+        expect(validCheck.isValid).toEqual(false)
+        expect(validCheck.errorMessages).toEqual(['degree must be a number', 'shape size is one'])
+    })
+
+    it('should be false when degree is over range', () => {
+        const args = ['node', 'matrix', '-p=pyramid', '-s=!', '-d=-5']
+        const arg = new Argument(args, options)
+        const validCheck = arg.validationCheck
+        expect(validCheck.isValid).toEqual(false)
+        expect(validCheck.errorMessages).toEqual(['degree range is more than 0 and less than 30'])
+    })
+
+    it('should be false when get more than options', () => {
+        const args = ['node', 'matrix', '-p=pyramiddd', '-s=!', '-d=5', '-e=']
+        const arg = new Argument(args, options)
+        const validCheck = arg.validationCheck
+        expect(validCheck.isValid).toEqual(false)
+        expect(validCheck.errorMessages).toEqual(['check the patterns'])
+    })
+
+    it('should be false when duplicated options', () => {
+        const args = ['node', 'matrix', '-p=pyramid', '-s=!', '-d=5', '-s=@']
+        const arg = new Argument(args, options)
+        const validCheck = arg.validationCheck
+        expect(validCheck.isValid).toEqual(false)
+        expect(validCheck.errorMessages).toEqual(['Duplicated s options'])
+    })
+})
--- src/util/cli.ts
+++ src/util/cli.ts
@@ -1,3 +1,10 @@
+import { LeftDownwardRightTriangle } from '../patterns/leftDownwardRightTriangle'
+import { LeftUpwardRightTriangle } from '../patterns/leftUpwardRightTriangle'
+import { RightDownwardRightTriangle } from '../patterns/rightDownwardRightTriangle'
+import { RightUpwardRightTriangle } from '../patterns/rightUpwardRightTriangle'
+import { SquareMatrix } from '../squareMatrix'
+
+// @ts-ignore
 function usage(): void {
     console.log('AwesomeMatrix CLI')
     console.log('Usage: matrix [Option]')
@@ -8,91 +15,195 @@ function usage(): void {
     console.log('  -d=degree : Users can set the order of array (defaultValue -d=5)')
 }
 
-interface IOption {
+export interface IOption {
     name: string
-    short: string
-    type?: 'number' | 'string' | 'string[]'
-    value?: any
-    validation?: (value: any) => IValidation
+    shortcut: string
+    defaultValue?: string
+    validation?: (value: string) => IValidation
 }
 
 interface IValidation {
-    isValue?: boolean
+    isValid?: boolean
     errorMessage?: string
 }
 
-// export function parsingValue(options: IOption[]) {
-//     for (let opt in options) {
-//         const valType = typeof options[opt].type
-//
-//         if (valType === 'string' && options[opt].type === typeof 'string') {
-//
-//         } else if (valType === 'number' && options[opt].type === typeof 'number') {
-//
-//         } else {
-//             return options[opt].value
-//         }
-//     }
-// }
-
 export const options: IOption[] = [
-    { name: 'help', short: '-h', value: usage() },
+    { name: 'help', shortcut: '-h' },
     {
+        defaultValue: 'pyramid',
         name: 'pattern',
-        short: '-p=',
-        type: 'string[]',
-        value: 'pyramid' || 'rightUpward' || 'leftUpward' || 'rightDownward' || 'leftDownward'
+        shortcut: 'p',
+        validation: (pValue: string) => {
+            const patterns = ['pyramid', 'leftUpward', 'rightUpward', 'leftDownward', 'rightDownward']
+            for (const pattern of patterns) {
+                if (pattern === pValue) {
+                    return { isValid: true }
+                }
+            }
+            return { errorMessage: 'check the patterns' }
+        }
     },
     {
+        defaultValue: '5',
         name: 'degree',
-        short: '-d=',
-        type: 'number',
-        validation: (dValue: number) => {
+        shortcut: 'd',
+        validation: (value: string) => {
+            const dValue = parseInt(value, 10)
+
             if (isNaN(dValue)) {
                 return {
                     errorMessage: 'degree must be a number',
-                    isValue: false
+                    isValid: false
                 }
             } else if (dValue < 0 || dValue > 30) {
                 return {
                     errorMessage: 'degree range is more than 0 and less than 30'
                 }
             } else {
-                return { isValue: true }
+                return { isValid: true }
             }
-        },
-        value: 5
+        }
     },
     {
+        defaultValue: '@',
         name: 'shape',
-        short: '-s=',
-        type: 'string',
+        shortcut: 's',
         validation: (sValue: string) => {
             if (sValue.length > 1) {
                 return { errorMessage: 'shape size is one' }
             } else if (sValue === '') {
                 return { errorMessage: 'fill the shape type' }
             } else {
-                return { isValue: true }
+                return { isValid: true }
             }
-        },
-        value: '@'
+        }
     }
 ]
 
+interface IValidCheck {
+    isValid: boolean
+    errorMessages: string[]
+}
+
 export class Argument {
-    public readonly args: string[]
-    public readonly options: IOption[]
+    public readonly validationCheck: IValidCheck
+    private readonly args: string[]
+    private readonly options: IOption[]
 
     constructor(args: string[], opt: IOption[]) {
         this.args = args
         this.options = opt
+        this.validationCheck = { isValid: true, errorMessages: [] }
+        this.evaluation()
+    }
+
+    public getValue(shortcut: string): string {
+        for (const arg of this.args) {
+            const arr: string[] = arg.split('=')
+
+            if (removeDash(arr[0]) === shortcut) {
+                return arr[1]
+            }
+        }
+
+        const defaultValue = this.getDefaultValue(shortcut)
+
+        return defaultValue ? defaultValue : ''
+    }
+
+    public getDefaultValue(shortcut: string): string | undefined {
+        for (const option of this.options) {
+            if (!option.defaultValue) {
+                continue
+            }
+            if (option.shortcut === shortcut) {
+                return option.defaultValue
+            }
+        }
+        return undefined
+    }
+
+    private evaluation() {
+        this.optionsEval()
+        this.duplicationEval()
+    }
+
+    private optionsEval() {
+        for (const option of this.options) {
+            const value = this.getValue(option.shortcut)
+
+            if (!option.validation) {
+                continue
+            }
+
+            const validation = option.validation(value)
+
+            if (validation.isValid) {
+                continue
+            }
+
+            this.validationCheck.isValid = false
+
+            if (validation.errorMessage) {
+                this.validationCheck.errorMessages.push(validation.errorMessage)
+            }
+        }
+    }
+
+    private duplicationEval() {
+        const checkSet = new Set()
+        for (const arg of this.args) {
+            const param = getParameter(arg)
+
+            if (!checkSet.has(param)) {
+                checkSet.add(param)
+                continue
+            }
+            this.validationCheck.isValid = false
+            this.validationCheck.errorMessages.push(`Duplicated ${param} options`)
+        }
     }
 }
 
-// noinspection JSUnusedGlobalSymbols
-function cliStart(): void {
-    const args = new Argument(process.argv.slice(0), options)
-    console.log(args)
+export function getParameter(arg: string): string {
+    const temp = arg.split('=')
+    return removeDash(temp[0])
 }
-cliStart()
+
+export function removeDash(value: string): string {
+    const sliceIdx = value.indexOf('-')
+    return value.slice(sliceIdx + 1)
+}
+
+// noinspection JSUnusedGlobalSymbols
+export function cliStart(): void {
+    const args = new Argument(process.argv.slice(0), options)
+    const degree = parseInt(args.getValue('d'), 10)
+    const shape = args.getValue('s')
+    const pattern = args.getValue('p')
+    const validCheck = args.validationCheck
+
+    if (!validCheck.isValid) {
+        for (const errorMessage of validCheck.errorMessages) {
+            console.error(errorMessage)
+        }
+        process.exit(-1)
+    }
+
+    if (pattern === 'pyramid') {
+        const pyramid = new SquareMatrix(shape, new RightUpwardRightTriangle())
+        pyramid.executePattern(degree)
+    } else if (pattern === 'rightUpward') {
+        const rightUpward = new SquareMatrix(shape, new RightUpwardRightTriangle())
+        rightUpward.executePattern(degree)
+    } else if (pattern === 'leftUpward') {
+        const leftUpward = new SquareMatrix(shape, new LeftUpwardRightTriangle())
+        leftUpward.executePattern(degree)
+    } else if (pattern === 'rightDownward') {
+        const rightDownward = new SquareMatrix(shape, new RightDownwardRightTriangle())
+        rightDownward.executePattern(degree)
+    } else if (pattern === 'leftDownward') {
+        const leftDownward = new SquareMatrix(shape, new LeftDownwardRightTriangle())
+        leftDownward.executePattern(degree)
+    }
+}
